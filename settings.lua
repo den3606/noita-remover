264edb35843732e4098e147ffb7085d7a4541edc
den3606 <dedendendedenpunn@gmail.com>
2023-05-06 17:17:04 +0900

    fix: Steam Workshopで設定画面が表示されない問題を修正した
    Steam Workshop経由でMODをInstallすると、Steam管理下におくための番号がMODに振られます。
    振られた番号とMODの名前の紐づけはNoitaのゲーム開始時に行わるため、
    Noitaを始める前のSettingsページにて、MOD内のフォルダを参照しようとすると
    Steam番号でフォルダ名が変更されているので、正しく読み込めない状態でした。

--- settings.lua
+++ settings.lua
@@ -50,12 +50,305 @@ function ModSettingsGuiCount()
   return mod_settings_gui_count(mod_id, mod_settings)
 end
 
+---------------------------------------------------------
+---------------------------------------------------------
+--------------------- NOITA REMOVER ---------------------
+---------------------------------------------------------
+---------------------------------------------------------
+
+
+---------------------------------------------------------
+-- VALUES.lua
+local VALUES = {
+  MOD_NAME = 'noita-remover',
+  GLOBAL_GUI_ID_KEY = 'noita-remover.global-gui-id-key',
+}
+---------------------------------------------------------
+
+
+
+---------------------------------------------------------
+-- gui_utils.lua
+function StringToNumber(str)
+  local num = 0
+  for i = 1, #str do
+    local char = string.sub(str, i, i)
+    num = num + string.byte(char)
+  end
+  return num
+end
+
+function NewID()
+  local global_gui_id = tonumber(ModSettingGet(VALUES.GLOBAL_GUI_ID_KEY)) or 0
+  if global_gui_id == 0 then
+    global_gui_id = StringToNumber(VALUES.MOD_NAME)
+  end
+
+  global_gui_id = global_gui_id + 1
+  ModSettingSet(VALUES.GLOBAL_GUI_ID_KEY, tostring(global_gui_id))
+  return global_gui_id
+end
+
+-- HACK: 画像とボタンテキストを重ねて設置している
+-- 各言語によってスペースの扱い勝ちが言うので、調整が必要
+function GuiToggleImageButton(gui, image_id, button_id, icon_path, state_name, active_fn, deactive_fn)
+  local w, h = GuiGetImageDimensions(gui, icon_path, 1)
+
+  local blank = ''
+
+  if GameTextGet("$current_language") == "English" then
+    for i = 0, math.floor(w / 4) do
+      blank = blank .. ' '
+    end
+  end
+  if GameTextGet("$current_language") == "日本語" then
+    for i = 0, math.floor(w / 2) do
+      blank = blank .. ' '
+    end
+  end
+
+
+  local is_active = ModSettingGet(state_name) or false
+
+  if GuiButton(gui, button_id, 0, h / 4, blank) then
+    is_active = not is_active
+
+    -- graphic処理以外はButtonが押されたときのみ動作させる
+    ModSettingSet(state_name, is_active)
+    if is_active then
+      active_fn()
+    else
+      deactive_fn()
+    end
+  end
+
+  if is_active then
+    GuiImage(gui, image_id, -w, 0, icon_path, 1, 1)
+  else
+    GuiImage(gui, image_id, -w, 0, icon_path, 0.3, 1)
+  end
+end
+
+---------------------------------------------------------
+
+
+
+---------------------------------------------------------
+-- perk_ui.lua
+dofile_once("data/scripts/perks/perk_list.lua")
+
+local perk_scroll_container_id = NewID()
+local remove_all_perk_button_id = NewID()
+local add_all_perk_button_id = NewID()
+local remove_random_perk_button_id = NewID()
+
+local perk_gui_rows = {}
+local perk_row = {}
+
+for i = 1, #perk_list do
+  table.insert(perk_row, {
+    key = VALUES.MOD_NAME .. perk_list[i].id,
+    image_id = NewID(),
+    button_id = NewID(),
+    icon_path = perk_list[i].perk_icon,
+    state_name = VALUES.MOD_NAME .. perk_list[i].perk_icon,
+    active_fn = function()
+      ModSettingSet(VALUES.MOD_NAME .. perk_list[i].id, true)
+    end,
+    deactive_fn = function()
+      ModSettingSet(VALUES.MOD_NAME .. perk_list[i].id, false)
+    end,
+  })
+  if i % 6 == 0 then
+    table.insert(perk_gui_rows, perk_row)
+    perk_row = {}
+  end
+end
+-- 最後に割り切れなかったperksを挿入する
+table.insert(perk_gui_rows, perk_row)
+
+
+local function perk_icon(gui)
+  for _, row in ipairs(perk_gui_rows) do
+    GuiLayoutBeginHorizontal(gui, 0, 0, false, 3);
+
+    for _, perk in ipairs(row) do
+      GuiToggleImageButton(gui, perk.image_id, perk.button_id, perk.icon_path, perk.state_name,
+        perk.active_fn, perk.deactive_fn)
+    end
+
+    GuiLayoutEnd(gui)
+  end
+end
+---------------------------------------------------------
+
+
+---------------------------------------------------------
+-- spell_ui.lua
+dofile("data/scripts/gun/gun_actions.lua")
+
+local spell_scroll_container_id = NewID()
+local remove_all_spell_button_id = NewID()
+local add_all_spell_button_id = NewID()
+local remove_random_spell_button_id = NewID()
+
+local spell_gui_rows = {}
+local spell_row = {}
+
+for i = 1, #actions do
+  table.insert(spell_row, {
+    key = VALUES.MOD_NAME .. actions[i].id,
+    image_id = NewID(),
+    button_id = NewID(),
+    icon_path = actions[i].sprite,
+    state_name = VALUES.MOD_NAME .. actions[i].sprite,
+    active_fn = function()
+      ModSettingSet(VALUES.MOD_NAME .. actions[i].id, true)
+    end,
+    deactive_fn = function()
+      ModSettingSet(VALUES.MOD_NAME .. actions[i].id, false)
+    end,
+  })
+  if i % 6 == 0 then
+    table.insert(spell_gui_rows, spell_row)
+    spell_row = {}
+  end
+end
+-- 最後に割り切れなかったspellsを挿入する
+table.insert(spell_gui_rows, spell_row)
+
+
+local function spell_icon(gui)
+  for _, row in ipairs(spell_gui_rows) do
+    GuiLayoutBeginHorizontal(gui, 0, 0, false, 3);
+
+    for _, spell in ipairs(row) do
+      GuiToggleImageButton(gui, spell.image_id, spell.button_id, spell.icon_path, spell.state_name,
+        spell.active_fn, spell.deactive_fn)
+    end
+
+    GuiLayoutEnd(gui)
+  end
+end
+---------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
 function ModSettingsGui(gui, in_main_menu)
   mod_settings_gui(mod_id, mod_settings, gui, in_main_menu)
 
-  local perk_ui = dofile_once("mods/noita-remover/files/scripts/ui/perk_ui.lua")
-  perk_ui.start(gui)
+  ---------------------------------------------------------
+  -- perk_ui.lua MAIN PROCESS
+  GuiLayoutBeginLayer(gui)
+  GuiBeginScrollContainer(gui, perk_scroll_container_id, 2, 42, 125, 278)
+  GuiLayoutBeginVertical(gui, 0, 0)
 
-  local spell_ui = dofile_once("mods/noita-remover/files/scripts/ui/spell_ui.lua")
-  spell_ui.start(gui)
+  -- In Box rendering
+  GuiText(gui, 0, 0, "Perk Ban List")
+  GuiText(gui, 0, 0, "=========================")
+
+  if GuiButton(gui, remove_random_perk_button_id, 0, 0, "Ban Random Perk") then
+    local unremoved_perks = {}
+    for _, row in ipairs(perk_gui_rows) do
+      for _, perk in ipairs(row) do
+        if (not ModSettingGet(perk.key)) or false then
+          table.insert(unremoved_perks, perk)
+        end
+      end
+    end
+
+    local ban_perk_number = math.random(#unremoved_perks)
+    for index, perk in ipairs(unremoved_perks) do
+      if index == ban_perk_number then
+        ModSettingSet(perk.state_name, true)
+        ModSettingSet(perk.key, true)
+      end
+    end
+  end
+
+  GuiText(gui, 0, 0, "-------------------------")
+
+  if GuiButton(gui, remove_all_perk_button_id, 0, 0, "Ban All Perks") then
+    for _, row in ipairs(perk_gui_rows) do
+      for _, perk in ipairs(row) do
+        ModSettingSet(perk.state_name, true)
+        ModSettingSet(perk.key, true)
+      end
+    end
+  end
+  if GuiButton(gui, add_all_perk_button_id, 0, 0, "Unban All Perks") then
+    for _, row in ipairs(perk_gui_rows) do
+      for _, perk in ipairs(row) do
+        ModSettingSet(perk.state_name, false)
+        ModSettingSet(perk.key, false)
+      end
+    end
+  end
+  perk_icon(gui)
+
+  GuiLayoutEnd(gui)
+  GuiEndScrollContainer(gui)
+  GuiLayoutEndLayer(gui)
+  ---------------------------------------------------------
+
+
+  ---------------------------------------------------------
+  -- spell_ui.lua MAIN PROCESS
+  GuiLayoutBeginLayer(gui)
+  GuiBeginScrollContainer(gui, spell_scroll_container_id, 500, 42, 125, 278)
+  GuiLayoutBeginVertical(gui, 0, 0)
+
+  -- In Box rendering
+  GuiText(gui, 0, 0, "Spell Ban List")
+  GuiText(gui, 0, 0, "=========================")
+
+  if GuiButton(gui, remove_random_spell_button_id, 0, 0, "Ban Random Spells") then
+    local unremoved_spells = {}
+    for _, row in ipairs(spell_gui_rows) do
+      for _, spell in ipairs(row) do
+        if (not ModSettingGet(spell.key)) or false then
+          table.insert(unremoved_spells, spell)
+        end
+      end
+    end
+
+    local ban_spell_number = math.random(#unremoved_spells)
+    for index, spell in ipairs(unremoved_spells) do
+      if index == ban_spell_number then
+        ModSettingSet(spell.state_name, true)
+        ModSettingSet(spell.key, true)
+      end
+    end
+  end
+
+  GuiText(gui, 0, 0, "-------------------------")
+
+  if GuiButton(gui, remove_all_spell_button_id, 0, 0, "Ban All spells") then
+    for _, row in ipairs(spell_gui_rows) do
+      for _, spell in ipairs(row) do
+        ModSettingSet(spell.state_name, true)
+        ModSettingSet(spell.key, true)
+      end
+    end
+  end
+  if GuiButton(gui, add_all_spell_button_id, 0, 0, "Unban All spells") then
+    for _, row in ipairs(spell_gui_rows) do
+      for _, spell in ipairs(row) do
+        ModSettingSet(spell.state_name, false)
+        ModSettingSet(spell.key, false)
+      end
+    end
+  end
+  spell_icon(gui)
+
+  GuiLayoutEnd(gui)
+  GuiEndScrollContainer(gui)
+  GuiLayoutEndLayer(gui)
 end
