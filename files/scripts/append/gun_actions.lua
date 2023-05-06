8a11a68658e36644d81173777d70a3e8454c691c
den3606 <dedendendedenpunn@gmail.com>
2023-05-06 17:30:13 +0900

    feat: dummyデータを入れた

--- files/scripts/append/gun_actions.lua
+++ files/scripts/append/gun_actions.lua
@@ -10,9 +10,20 @@ for i = #actions, 1, -1 do
   end
 end
 
-
--- for i = #actions, 1, -1 do
---   if i ~= 1 then
---     table.remove(actions, i)
---   end
--- end
+if #actions == 0 then
+  table.insert(actions, {
+    id = "DUMMY",
+    name = "DUMMY",
+    description = "DUMMY",
+    sprite = "",
+    sprite_unidentified = "",
+    type = ACTION_TYPE_PROJECTILE,
+    spawn_level = "",
+    spawn_probability = "",
+    price = 0,
+    mana = 0,
+    max_uses = 0,
+    action = function()
+    end,
+  })
+end
