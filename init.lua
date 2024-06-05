47c8a7b817f83c709a3e960201a0f6a840dcb47f
den3606 <dedendendedenpunn@gmail.com>
2024-06-06 03:22:04 +0900

    refactor: move add process to function

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 local VALUES = dofile_once("mods/noita-remover/files/scripts/variables.lua")
 local Json = dofile_once("mods/noita-remover/files/scripts/lib/jsonlua/json.lua")
+
 ModLuaFileAppend("data/scripts/perks/perk_list.lua",
   "mods/noita-remover/files/scripts/append/perk_list.lua")
 ModLuaFileAppend("data/scripts/gun/gun_actions.lua",
@@ -31,53 +32,57 @@ end
 function OnWorldInitialized() -- This is called once the game world is initialized. Doesn't ensure any world chunks actually exist. Use OnPlayerSpawned to ensure the chunks around player have been loaded or created.
   local want_to_refresh = ModSettingGet(VALUES.WANT_TO_RELOAD_KEY) or true
   if want_to_refresh then
-    dofile("data/scripts/perks/perk_list.lua")
-    dofile("data/scripts/gun/gun_actions.lua")
-
     -- Extract Modding Spells from source
-    local noita_remover_spells = Json.decode(ModSettingGet(VALUES.SPELL_BAN_LIST_KEY) or "{}")
-    local is_updated = false
-    for _, action in ipairs(actions) do
-      local is_newer = true
-      for _, noita_remover_spell in ipairs(noita_remover_spells) do
-        if action.id == noita_remover_spell.id then
-          is_newer = false
-          break
+    local function add_spells()
+      dofile("data/scripts/gun/gun_actions.lua")
+      local noita_remover_spells = Json.decode(ModSettingGet(VALUES.SPELL_BAN_LIST_KEY) or "{}")
+      local is_updated = false
+      for _, action in ipairs(actions) do
+        local is_newer = true
+        for _, noita_remover_spell in ipairs(noita_remover_spells) do
+          if action.id == noita_remover_spell.id then
+            is_newer = false
+            break
+          end
+        end
+        if is_newer then
+          is_updated = true
+          table.insert(noita_remover_spells, { id = action.id, sprite = action.sprite })
         end
       end
-      if is_newer then
-        is_updated = true
-        table.insert(noita_remover_spells, { id = action.id, sprite = action.sprite })
+
+      if is_updated then
+        local serialized_noita_remover_spells = Json.encode(noita_remover_spells)
+        ModSettingSet(VALUES.SPELL_BAN_LIST_KEY, serialized_noita_remover_spells)
       end
     end
-
-    if is_updated then
-      local serialized_noita_remover_spells = Json.encode(noita_remover_spells)
-      ModSettingSet(VALUES.SPELL_BAN_LIST_KEY, serialized_noita_remover_spells)
-    end
-
+    add_spells()
 
     -- Extract Modding perks from source
-    local noita_remover_perks = Json.decode(ModSettingGet(VALUES.PERK_BAN_LIST_KEY) or "{}")
-    local is_updated = false
-    for _, perk in ipairs(perk_list) do
-      local is_newer = true
-      for _, noita_remover_perk in ipairs(noita_remover_perks) do
-        if perk.id == noita_remover_perk.id then
-          is_newer = false
-          break
+    local function add_perks()
+      dofile("data/scripts/perks/perk_list.lua")
+      local noita_remover_perks = Json.decode(ModSettingGet(VALUES.PERK_BAN_LIST_KEY) or "{}")
+      local is_updated = false
+      for _, perk in ipairs(perk_list) do
+        local is_newer = true
+        for _, noita_remover_perk in ipairs(noita_remover_perks) do
+          if perk.id == noita_remover_perk.id then
+            is_newer = false
+            break
+          end
+        end
+        if is_newer then
+          is_updated = true
+          table.insert(noita_remover_perks, { id = perk.id, perk_icon = perk.perk_icon })
         end
       end
-      if is_newer then
-        is_updated = true
-        table.insert(noita_remover_perks, { id = perk.id, perk_icon = perk.perk_icon })
+
+      if is_updated then
+        local serialized_noita_remover_perks = Json.encode(noita_remover_perks)
+        ModSettingSet(VALUES.PERK_BAN_LIST_KEY, serialized_noita_remover_perks)
       end
     end
-
-    if is_updated then
-      local serialized_noita_remover_perks = Json.encode(noita_remover_perks)
-      ModSettingSet(VALUES.PERK_BAN_LIST_KEY, serialized_noita_remover_perks)
-    end
+    add_perks()
 
     ModSettingSet(VALUES.WANT_TO_RELOAD_KEY, false)
   end
