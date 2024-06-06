a97f1f222c0d5c60b0f422936e78df57f3325208
den3606 <dedendendedenpunn@gmail.com>
2024-06-07 07:02:41 +0900

    feat: support for subsequent additions and add process to show dummy perk

--- files/scripts/append/gun_actions.lua
+++ files/scripts/append/gun_actions.lua
@@ -1,10 +1,10 @@
 local VALUES = dofile_once("mods/noita-remover/files/scripts/variables.lua")
+local Json = dofile_once("mods/noita-remover/files/scripts/lib/jsonlua/json.lua")
 
 -- NOTE:
 -- 初回の読み込みはNoitaの構築前に実施されるので、
 -- actionsのデータだけ先に吸い出す
 local function add_spells()
-  local Json = dofile_once("mods/noita-remover/files/scripts/lib/jsonlua/json.lua")
   local noita_remover_spells = Json.decode(ModSettingGet(VALUES.SPELL_BAN_LIST_KEY) or "{}")
   local is_updated = false
   for _, action in ipairs(actions) do
@@ -54,11 +54,63 @@ if #actions == 0 then
     action = function()
     end,
   })
+  AddFlagPersistent("action_noita_remover_dummy")
 end
 if #actions > 1 then
   for index, action in ipairs(actions) do
     if action.id == "NOITA_REMOVER_DUMMY" then
       table.remove(actions, index)
     end
+  end
+end
+
+
+-- MOD初期化以降に追加されるスペル対応
+local mt = {
+  __newindex = function(t, key, value)
+    local noita_remover_spells = Json.decode(ModSettingGet(VALUES.SPELL_BAN_LIST_KEY) or "{}")
+    local is_updated = false
+    local is_newer = true
+    for _, noita_remover_spell in ipairs(noita_remover_spells) do
+      if value.id == noita_remover_spell.id then
+        is_newer = false
+        break
+      end
+    end
+    if is_newer then
+      is_updated = true
+      table.insert(noita_remover_spells, { id = value.id, sprite = value.sprite })
+    end
+
+
+    if is_updated then
+      local serialized_noita_remover_spells = Json.encode(noita_remover_spells)
+      ModSettingSet(VALUES.SPELL_BAN_LIST_KEY, serialized_noita_remover_spells)
+    end
+
+
+    -- BANされている場合はactionsからの削除（追加回避）
+    local banned = ModSettingGet(VALUES.SPELL_BAN_PREFIX .. value.id) or false
+    if not banned then
+      rawset(t, key, value)
+    end
+  end
+}
+setmetatable(actions, mt)
+
+-- table.insertはmetadataに反応しないため再実装する
+table.insert = function(t, pos, value)
+  if value == nil then
+    value = pos
+    pos = #t + 1
+  end
+
+  if pos > #t then
+    t[pos] = value
+  else
+    for i = #t, pos, -1 do
+      t[i + 1] = t[i]
+    end
+    t[pos] = value
   end
 end
--- files/scripts/append/perk_list.lua
+++ files/scripts/append/perk_list.lua
@@ -1,11 +1,10 @@
 local VALUES = dofile_once("mods/noita-remover/files/scripts/variables.lua")
-
+local Json = dofile_once("mods/noita-remover/files/scripts/lib/jsonlua/json.lua")
 -- NOTE:
 -- 初回の読み込みはNoitaの構築前に実施されるので、
 -- actionsのデータだけ先に吸い出す
 -- Useless Perk等、PostMOD以降で読み込ませているものは読めない
 local function add_perks()
-  local Json = dofile_once("mods/noita-remover/files/scripts/lib/jsonlua/json.lua")
   local noita_remover_perks = Json.decode(ModSettingGet(VALUES.PERK_BAN_LIST_KEY) or "{}")
   local is_updated = false
   for _, perk in ipairs(perk_list) do
@@ -39,7 +38,6 @@ for i = #perk_list, 1, -1 do
   end
 end
 
-
 if #perk_list == 0 then
   table.insert(perk_list, {
     id = "NOITA_REMOVER_DUMMY",
@@ -52,11 +50,62 @@ if #perk_list == 0 then
     stackable = STACKABLE_NO,
     usable_by_enemies = false,
   })
+  AddFlagPersistent("perk_picked_noita_remover_dummy")
 end
 if #perk_list > 1 then
   for index, perk in ipairs(perk_list) do
     if perk.id == "NOITA_REMOVER_DUMMY" then
       table.remove(perk_list, index)
     end
+  end
+end
+
+-- MOD初期化以降に追加されるパーク対応
+local mt = {
+  __newindex = function(t, key, value)
+    -- noita-removerのPerk一覧に追加する
+    local noita_remover_perks = Json.decode(ModSettingGet(VALUES.PERK_BAN_LIST_KEY) or "{}")
+    local is_updated = false
+    local is_newer = true
+    for _, noita_remover_perk in ipairs(noita_remover_perks) do
+      if value.id == noita_remover_perk.id then
+        is_newer = false
+        break
+      end
+    end
+    if is_newer then
+      is_updated = true
+      table.insert(noita_remover_perks, { id = value.id, perk_icon = value.perk_icon })
+    end
+
+    if is_updated then
+      local serialized_noita_remover_perks = Json.encode(noita_remover_perks)
+      ModSettingSet(VALUES.PERK_BAN_LIST_KEY, serialized_noita_remover_perks)
+    end
+
+
+    -- BANされている場合はperk_listからの削除（追加回避）
+    local banned = ModSettingGet(VALUES.PERK_BAN_PREFIX .. value.id) or false
+    if not banned then
+      rawset(t, key, value)
+    end
+  end
+}
+setmetatable(perk_list, mt)
+
+-- table.insertはmetadataに反応しないため再実装する
+table.insert = function(t, pos, value)
+  if value == nil then
+    value = pos
+    pos = #t + 1
+  end
+
+  if pos > #t then
+    t[pos] = value
+  else
+    for i = #t, pos, -1 do
+      t[i + 1] = t[i]
+    end
+    t[pos] = value
   end
 end
